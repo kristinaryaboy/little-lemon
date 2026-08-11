95675ea1970e973e00a5d6ae375802e1360d65e9
Kristina Ryaboy <kristinaryaboy13@gmail.com>
2026-08-11 13:55:07 -0400

    Add images and wire up logo in Header

---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+function Footer() {
+  return (
+    <footer>
+      <p>Little Lemon</p>
+      <p>Chicago, IL</p>
+      <nav>
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#menu">Menu</a>
+      </nav>
+      <p>&copy; 2026 Little Lemon. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import logo from './images/Logo1.jpg';
+
+function Header() {
+  return (
+    <header>
+      <img src={logo} alt="Little Lemon logo" />
+    </header>
+  );
+}
+
+export default Header;
